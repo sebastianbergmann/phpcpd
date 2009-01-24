bbbf1324673c4578490c69e4091d1cf3e795078c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-01-24 17:22:12 +0100

    Start project.

---
+++ PHPCPD/TextUI/Command.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * phpcpd
+ *
+ * Copyright (c) 2009, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package   phpcpd
+ * @author    Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright 2009 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @since     File available since Release 1.0.0
+ */
+
+require 'PHPCPD/Detector.php';
+require 'PHPCPD/TextUI/Getopt.php';
+require 'PHPCPD/TextUI/ResultPrinter.php';
+require 'PHPCPD/Util/FilterIterator.php';
+
+/**
+ *
+ *
+ * @author    Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright 2009 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://github.com/sebastianbergmann/phpcpd/tree
+ * @since     Class available since Release 1.0.0
+ */
+class PHPCPD_TextUI_Command
+{
+    public static function main()
+    {
+        try {
+            $options = PHPCPD_TextUI_Getopt::getopt(
+              $_SERVER['argv'],
+              '',
+              array(
+                'help',
+                'suffixes=',
+                'version'
+              )
+            );
+        }
+
+        catch (RuntimeException $e) {
+            self::showError($e->getMessage());
+        }
+
+        $suffixes = array('php');
+
+        foreach ($options[0] as $option) {
+            switch ($option[0]) {
+                case '--suffixes': {
+                    $suffixes = explode(',', $option[1]);
+                    array_map('trim', $suffixes);
+                }
+                break;
+
+                case '--help': {
+                    self::showHelp();
+                    exit(0);
+                }
+                break;
+
+                case '--version': {
+                    self::printVersionString();
+                    exit(0);
+                }
+                break;
+            }
+        }
+
+        if (isset($options[1][0])) {
+            if (is_dir($options[1][0])) {
+                $files = new PHPCPD_Util_FilterIterator(
+                  new RecursiveIteratorIterator(
+                    new RecursiveDirectoryIterator($options[1][0])
+                  ),
+                  $suffixes
+                );
+            }
+
+            else if (is_file($options[1][0])) {
+                $files = array(new SPLFileInfo($options[1][0]));
+            }
+        }
+
+        if (isset($files)) {
+            // ...
+        } else {
+            self::showHelp();
+        }
+    }
+
+    /**
+     * Shows an error.
+     */
+    protected static function showError($message)
+    {
+        self::printVersionString();
+
+        print $message;
+
+        exit(1);
+    }
+
+    /**
+     * Shows the help.
+     */
+    protected static function showHelp()
+    {
+        self::printVersionString();
+
+        print <<<EOT
+Usage: phpcpd [switches] <directory>
+       phpcpd [switches] <file>
+
+  --suffixes <suffix,...>  A comma-separated list of file suffixes to check.
+
+  --help                   Prints this usage information.
+  --version                Prints the version and exits.
+
+EOT;
+    }
+
+    /**
+     * Prints the version string.
+     */
+    protected static function printVersionString()
+    {
+        print "phpcpd @package_version@ by Sebastian Bergmann.\n\n";
+    }
+}
+?>
---
+++ README.markdown
@@ -0,0 +1,22 @@
+phpcpd
+======
+
+**phpcpd** is a Copy/Paste Detector (CPD) for PHP code.
+
+The goal of **phpcpd** is not not to replace more sophisticated tools such as [phpcs](http://pear.php.net/PHP_CodeSniffer), [pdepend](http://pdepend.org/), or [phpmd](http://phpmd.org/), but rather to provide an alternative to them when you just need to get a quick overview of duplicated code in a project.
+
+Requirements
+------------
+
+* The [tokenizer](http://www.php.net/tokenizer) extension is required.
+
+Usage Example
+-------------
+
+    sb@ubuntu phpcpd % ./phpcpd.php /path/to/project
+    phpcpd 1.0.0 by Sebastian Bergmann.
+
+    .
+    .
+    .
+
