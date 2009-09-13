d8656613c2cb23e088ef2076a257fe59dfe172d7
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-09-13 16:25:50 +0200

    Fix CS.

--- PHPCPD/TextUI/Getopt.php
+++ PHPCPD/TextUI/Getopt.php
@@ -52,7 +52,8 @@
  * @link      http://github.com/sebastianbergmann/phpcpd/tree
  * @since     Class available since Release 1.0.0
  */
-class PHPCPD_TextUI_Getopt {
+class PHPCPD_TextUI_Getopt
+{
     public static function getopt(array $args, $short_options, $long_options = NULL)
     {
         if (empty($args)) {
