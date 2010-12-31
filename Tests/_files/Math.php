9f3cb3375ff70cd8b650b199ecb09b496e2f9431
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-12-31 11:44:02 +0100

    Bump copyright year.

--- Tests/_files/Math.php
+++ Tests/_files/Math.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of phpUnderControl.
  *
- * Copyright (c) 2007-2010, Manuel Pichler <mapi@phpundercontrol.org>.
+ * Copyright (c) 2007-2011, Manuel Pichler <mapi@phpundercontrol.org>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -36,7 +36,7 @@
  *
  * @package   Example
  * @author    Manuel Pichler <mapi@phpundercontrol.org>
- * @copyright 2007-2010 Manuel Pichler. All rights reserved.
+ * @copyright 2007-2011 Manuel Pichler. All rights reserved.
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version   SVN: $Id$
  * @link      http://www.phpundercontrol.org/
@@ -47,7 +47,7 @@
  *
  * @package   Example
  * @author    Manuel Pichler <mapi@phpundercontrol.org>
- * @copyright 2007-2010 Manuel Pichler. All rights reserved.
+ * @copyright 2007-2011 Manuel Pichler. All rights reserved.
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version   Release: @package_version@
  * @link      http://www.phpundercontrol.org/
