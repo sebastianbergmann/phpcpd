47b348c40d7d8d204869791277be0282667de7ae
Arnout Boks <arnoutboks@gmail.com>
2013-05-24 11:52:50 +0200

    Changed expected duplication size in DetectorTest
    The default detection strategy currently does not count the number of tokens
    in a duplication correctly; it neglects the last ($minTokens - 2) tokens. This
    commit changes DetectorTest to expect the detector to find the duplication in
    Math.php to its full extent.

--- tests/DetectorTest.php
+++ tests/DetectorTest.php
@@ -78,8 +78,8 @@ class PHPCPD_DetectorTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(85, $clones[0]->aStartLine);
         $this->assertEquals(TEST_FILES_PATH . 'Math.php', $clones[0]->bFile);
         $this->assertEquals(149, $clones[0]->bStartLine);
-        $this->assertEquals(28, $clones[0]->size);
-        $this->assertEquals(68, $clones[0]->tokens);
+        $this->assertEquals(59, $clones[0]->size);
+        $this->assertEquals(136, $clones[0]->tokens);
 
         $this->assertEquals(
           '    public function div($v1, $v2)
@@ -110,6 +110,37 @@ class PHPCPD_DetectorTest extends PHPUnit_Framework_TestCase
         foreach ( $d as $x )
         {
             $v8 *= $x;
+        }
+
+        $v3 = $v1 / ($v2 + $v1);
+        if ($v3 > 14)
+        {
+            $v4 = 0;
+            for ($i = 0; $i < $v3; $i++)
+            {
+                $v4 += ($v2 * $i);
+            }
+        }
+        $v5 = ($v4 < $v3 ? ($v3 - $v4) : ($v4 - $v3));
+
+        $v6 = ($v1 * $v2 * $v3 * $v4 * $v5);
+
+        $d = array($v1, $v2, $v3, $v4, $v5, $v6);
+
+        $v7 = 1;
+        for ($i = 0; $i < $v6; $i++)
+        {
+            shuffle( $d );
+            $v7 = $v7 + $i * end($d);
+        }
+
+        $v8 = $v7;
+        foreach ( $d as $x )
+        {
+            $v8 *= $x;
+        }
+
+        return $v8;
 ',
           $clones[0]->getLines()
         );
