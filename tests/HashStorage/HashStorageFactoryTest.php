3aeb6c5911c8a4901904e56dd6cf50e9b81d6fa7
Matthias Glaub <magl@magl.net>
2014-04-13 12:12:14 +0200

    testing multiple objects/arrays in SQLite HashStorage reformatting test cases

--- tests/HashStorage/HashStorageFactoryTest.php
+++ tests/HashStorage/HashStorageFactoryTest.php
@@ -6,22 +6,22 @@ use SebastianBergmann\PHPCPD\Detector\Adapter\HashStorage\HashStorageFactory;
  * @author Matthias Glaub <magl@magl.net>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  */
-class HashStorageFactoryTest extends \PHPUnit_Framework_TestCase
+class PHPCPD_Adapter_HashStorageFactoryTest extends \PHPUnit_Framework_TestCase
 {
-	public function testDefaults(){
-		
-		$storageAdapter = HashStorageFactory::createStorageAdapter();
-		
-		$this->assertInstanceOf('SebastianBergmann\PHPCPD\Detector\Adapter\HashStorage\HashStorageInterface', $storageAdapter);
-		$this->assertInstanceOf('SebastianBergmann\PHPCPD\Detector\Adapter\HashStorage\Memory', $storageAdapter);
-		
-	}
-	
-	public function testUnknownAdapter(){
-		
-		$this->setExpectedException('InvalidArgumentException');
-		$storageAdapter = HashStorageFactory::createStorageAdapter('doesnotexist');
-		
-	}
 
+    public function testDefaults()
+    {
+
+        $storageAdapter = HashStorageFactory::createStorageAdapter();
+
+        $this->assertInstanceOf('SebastianBergmann\PHPCPD\Detector\Adapter\HashStorage\HashStorageInterface', $storageAdapter);
+        $this->assertInstanceOf('SebastianBergmann\PHPCPD\Detector\Adapter\HashStorage\Memory', $storageAdapter);
+    }
+
+    public function testUnknownAdapter()
+    {
+
+        $this->setExpectedException('InvalidArgumentException');
+        $storageAdapter = HashStorageFactory::createStorageAdapter('doesnotexist');
+    }
 }
