d3ad100fdf15805495f6ff19f473f4314c99390c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-03-26 15:47:38 +0100

    Prepare release

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -30,7 +30,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('2.0.1', dirname(dirname(__DIR__)));
+        $version = new Version('2.0.2', dirname(dirname(__DIR__)));
         parent::__construct('phpcpd', $version->getVersion());
     }
 
